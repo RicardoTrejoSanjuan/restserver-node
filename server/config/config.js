61fd9e78046b8fa08f8cfa96c1c280fb180400e1
Ricardo Trejo Sanjuan <ricardo.trejo@stratplus.net>
2019-05-14 15:16:32 -0500

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -19,7 +19,7 @@ let urlDB;
 if (process.env.NODE_ENV == 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://Rick:LgcDS7SNfYk6LXr3@cluster0-fojym.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
